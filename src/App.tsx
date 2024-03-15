272f895e5da6852f43e37a3eb49eb7f508addde4
GeoFrickz <george.maximillian@gmail.com>
2024-03-15 14:53:31 +0700

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,59 @@
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import PokemonList from './Page/PokemonList';
-import PokemonDetails from './Page/PokemonDetails';
-import { useEffect, useState } from 'react';
-import { IPokemonProps } from './Component/IPokemon';
-import { IPokemonDetailsProps } from './Component/IPokemonDetails';
-import axios from 'axios';
-
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PokemonList from "./Page/PokemonList";
+import PokemonDetails from "./Page/PokemonDetails";
+import { useEffect, useState } from "react";
+import { IPokemonProps } from "./Component/IPokemon";
+import { IPokemonDetailsProps } from "./Component/IPokemonDetails";
+import axios from "axios";
 
 function App() {
   const [pokemonList, setPokemonList] = useState<IPokemonProps[]>([]);
-  const [ pokemonDetails, setPokemonDetails ] = useState<IPokemonDetailsProps[]>([]);
-  const [ API, setAPI ] = useState<string | undefined>(process.env.REACT_APP_BASE_URL);
+  const [pokemonDetails, setPokemonDetails] = useState<IPokemonDetailsProps[]>(
+    []
+  );
 
   useEffect(() => {
-      const fetchData = async () => {
-        try {
-          if(API){
-            const response = await axios.get(API);
-            const { results } = await response.data;
-            setPokemonList(results);
-          }
-        } catch (error) {
-          console.log("Error fetching data: ", error);
-        }
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_BASE_URL!);
+        const { results } = await response.data;
+        setPokemonList(results);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
       }
-      fetchData();
-  }, [API]);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
-      const fetchPokemonDetails = async () => {
-          const details = await Promise.all(pokemonList.map(async (pokemon: { url: string; }) => {
-              const response = await axios.get(pokemon.url);
-              const { name, id, weight, height, moves, sprites } = response.data;
-              const sprite = sprites.front_default;
-              const official = sprites.other['official-artwork']['front_default'];
-              return { official, name, id, weight, height, moves, sprite };
-          }))
-          setPokemonDetails(details);
-      }
-      fetchPokemonDetails();
+    const fetchPokemonDetails = async () => {
+      const details = await Promise.all(
+        pokemonList.map(async (pokemon: { url: string }) => {
+          const response = await axios.get(pokemon.url);
+          const { name, id, weight, height, moves, sprites } = response.data;
+          const sprite = sprites.front_default;
+          const official = sprites.other["official-artwork"]["front_default"];
+          return { official, name, id, weight, height, moves, sprite };
+        })
+      );
+      setPokemonDetails(details);
+    };
+    fetchPokemonDetails();
   }, [pokemonList]);
 
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<PokemonList pokemonDetails={pokemonDetails}/>
+      path: "/",
+      element: <PokemonList pokemonDetails={pokemonDetails} />,
     },
     {
-      path:"Details/:ID",
-      element:<PokemonDetails/>
-    }
-  ])
+      path: "Details/:ID",
+      element: <PokemonDetails />,
+    },
+  ]);
 
-  return (
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Page/PokemonDetails.tsx
+++ src/Page/PokemonDetails.tsx
@@ -5,73 +5,93 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const PokemonDetails = () => {
+  const [pokemonDetails, setPokemonDetails] = useState<IPokemonDetailsProps>();
+  const { ID } = useParams<string>();
 
-    const [ pokemonDetails, setPokemonDetails ] = useState<IPokemonDetailsProps>();
-    const { ID } = useParams<string>();
-    const [ API, setAPI ] = useState<string | undefined>(process.env.REACT_APP_BASE_URL);
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}${ID}`
+        );
+        const { name, id, weight, height, moves, sprites } = response.data;
+        const sprite = sprites.front_default;
+        const official = sprites.other["official-artwork"]["front_default"];
+        setPokemonDetails({
+          official,
+          name,
+          id,
+          weight,
+          height,
+          moves,
+          sprite,
+        });
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    };
+    fetchPokemonDetails();
+  }, []);
 
-    useEffect(() => {
-        const fetchPokemonDetails = async () => {
-            try{
-                if(API){
-                    const response = await axios.get(`${API}${ID}`);
-                    const { name, id, weight, height, moves, sprites } = response.data;
-                    const sprite = sprites.front_default;
-                    const official = sprites.other['official-artwork']['front_default'];
-                    setPokemonDetails({ official, name, id, weight, height, moves, sprite });
-                }
-            }
-            catch (error){
-                console.log("Error fetching data: ", error);
-            }
-        }
-        fetchPokemonDetails();
-    }, [API]);
+  const formatID = (num: number) => {
+    return num.toString().padStart(4, "0");
+  };
 
-    const formatID = (num: number) => {
-        return num.toString().padStart(4, '0');
-    }
-
-    if(!pokemonDetails){
-        return(
-            <Card>
-                <CardContent>
-                    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                        <Typography variant="h2">Pokemon Not Found!</Typography>
-                    </Box>
-                </CardContent>
-            </Card>
-        )
-    }
-
-    const formattedID = formatID(pokemonDetails.id);
-
+  if (!pokemonDetails) {
     return (
-        <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
-            <Typography variant="h2" textTransform={"uppercase"} gutterBottom>{pokemonDetails?.name}</Typography>
-            <Box display={"flex"} gap={"50px"}>
-                <Box display={"flex"} flexDirection={"column"} gap={"50px"}>
-                    <Card sx={{width: '100%'}}><img src={pokemonDetails?.official} alt={pokemonDetails?.name}/></Card>
-                    <Card sx={{width: '100%'}}><img src={pokemonDetails?.sprite} alt={pokemonDetails?.name} width="100%"/></Card>
-                </Box>
-                <Card sx={{width: "300px"}}>
-                    <CardContent>
-                        <Box display={"flex"} flexDirection={"column"} gap={"10px"}>
-                            <Typography>Pokedex ID: #{formattedID}</Typography>
-                            <Typography>Height: {pokemonDetails.height}</Typography>
-                            <Typography>Weight: {pokemonDetails.weight}</Typography>
-                            <Typography>Move:</Typography>
-                            {pokemonDetails.moves.map((move, index) => (
-                                <Card>
-                                    <Typography>{move.move.name}</Typography>
-                                </Card>
-                            ))}
-                        </Box>
-                    </CardContent>
-                </Card>
-            </Box>
-        </Box>
-    )
-}
+      <Card>
+        <CardContent>
+          <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+            <Typography variant="h2">Pokemon Not Found!</Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    );
+  }
 
-export default PokemonDetails
+  const formattedID = formatID(pokemonDetails.id);
+
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <Typography variant="h2" textTransform={"uppercase"} gutterBottom>
+        {pokemonDetails?.name}
+      </Typography>
+      <Box display={"flex"} gap={"50px"}>
+        <Box display={"flex"} flexDirection={"column"} gap={"50px"}>
+          <Card sx={{ width: "100%" }}>
+            <img src={pokemonDetails?.official} alt={pokemonDetails?.name} />
+          </Card>
+          <Card sx={{ width: "100%" }}>
+            <img
+              src={pokemonDetails?.sprite}
+              alt={pokemonDetails?.name}
+              width="100%"
+            />
+          </Card>
+        </Box>
+        <Card sx={{ width: "300px" }}>
+          <CardContent>
+            <Box display={"flex"} flexDirection={"column"} gap={"10px"}>
+              <Typography>Pokedex ID: #{formattedID}</Typography>
+              <Typography>Height: {pokemonDetails.height}</Typography>
+              <Typography>Weight: {pokemonDetails.weight}</Typography>
+              <Typography>Move:</Typography>
+              {pokemonDetails.moves.map((move, index) => (
+                <Card>
+                  <Typography>{move.move.name}</Typography>
+                </Card>
+              ))}
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </Box>
+  );
+};
+
+export default PokemonDetails;
